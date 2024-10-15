a29cac07e64a7151ac4256359d9ce00c6b692244
Johann Hahn <wanische@gmail.com>
2024-10-15 12:36:55 +0200

    hi

--- sprite_paint.cpp
+++ sprite_paint.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
-#include "raylib"
+#include "includes/raylib.h"
 
 int main() {
     std::cout << "hi""\n";
+    InitWindow(900, 900, "hi");
+    while(!WindowShouldClose()) {
+	BeginDrawing();
+	ClearBackground(RED);
+	EndDrawing();
+    }
+    CloseWindow();
     return 0;
 }
