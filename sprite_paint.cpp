87402a77f301c9046a3e97835b842b07fbbafb13
Johann Hahn <johann-hahn@yandex.ru>
2024-10-18 17:47:17 +0200

    color picker

--- sprite_paint.cpp
+++ sprite_paint.cpp
@@ -6,6 +6,7 @@
 #include <stdint.h>
 typedef uint64_t u64;
 typedef uint32_t u32;
+typedef uint8_t u8;
 
 enum Mouse_Mode {
     DRAW, LINE, FILL, MOUSE_MODE_MAX
@@ -18,9 +19,17 @@ Rectangle rec_slice_vert(Rectangle rec, u64 slot, u64 max_slots) {
     return {rec.x, y, rec.width, height};
 }
 
+Rectangle rec_slice_horz(Rectangle rec, u64 slot, u64 max_slots) {
+    assert(slot < max_slots);
+    float width = rec.width / max_slots;
+    float x = rec.x + width * slot;
+    return {x, rec.y, width, rec.height};
+}
+
 struct Slider {
     float value;
     Rectangle boundary;
+    Rectangle handle_rec;
 };
 
 struct Color_Picker {
@@ -29,19 +38,29 @@ struct Color_Picker {
     Slider b;
     Slider a;
     Color to_color() {
-	return {r.value, g.value, b.value, a.value};
+	Color color = BLACK;
+	color.r = r.value * 255;
+	color.g = g.value * 255;
+	color.b = b.value * 255;
+	color.a = a.value * 255;
+	return color;
     }
     Rectangle boundary;
-    void setup(Rectangle boundary) {
+    void setup(Rectangle boundary, Color color) {
 	this->boundary = boundary;
-	r.value = 0.f;
-	g.value = 0.f;
-	b.value = 0.f;
-	a.value = 0.f;
+	r.value = color.r / 255.f;
+	g.value = color.g / 255.f;
+	b.value = color.b / 255.f;
+	a.value = color.a / 255.f;
+	float max_slot_hor = 20.f;
 	r.boundary = rec_slice_vert(boundary, 0, 5);  
+	r.handle_rec = rec_slice_horz(r.boundary, r.value * (max_slot_hor - 1), max_slot_hor);
 	g.boundary = rec_slice_vert(boundary, 1, 5);  
+	g.handle_rec = rec_slice_horz(g.boundary, g.value * (max_slot_hor - 1), max_slot_hor);
 	b.boundary = rec_slice_vert(boundary, 2, 5);  
+	b.handle_rec = rec_slice_horz(b.boundary, b.value * (max_slot_hor - 1), max_slot_hor);
 	a.boundary = rec_slice_vert(boundary, 3, 5);  
+	a.handle_rec = rec_slice_horz(a.boundary, a.value * (max_slot_hor - 1), max_slot_hor);
     }
 };
 
@@ -72,15 +91,12 @@ Rectangle squish_rec(Rectangle rec, float padding) {
     return {rec.x + padding, rec.y + padding, rec.width - padding * 2.f, rec.height - padding * 2.f};
 }
 
-
-
 Vector2 point_to_cell(Vector2 point, const Sprite& sprite) {
     point = Vector2Divide(point, {sprite.boundary.width, sprite.boundary.height});	
     point = Vector2Multiply(point, {(float)sprite.img.width, (float)sprite.img.height});
     point = {floor(point.x), floor(point.y)};
     return point; 
 }
-
 
 void init_window(Window& window) {
     InitWindow(window.width, window.height, window.title);
@@ -91,23 +107,26 @@ void draw_slider(const Slider& slider) {
     DrawRectangleLinesEx(slider.boundary, 2.f, WHITE);
     Rectangle sl_bg_rec = squish_rec(slider.boundary, 10.f);
     DrawRectangleRec(sl_bg_rec, WHITE);
-    float padding = 5.f;
-    Rectangle slider_rec = {sl_bg_rec.x + sl_bg_rec.width * slider.value, sl_bg_rec.y - padding, sl_bg_rec.width / 20.f, sl_bg_rec.height + padding * 2};
-    DrawRectangleRec(slider_rec, GRAY);
+    DrawRectangleRec(slider.handle_rec, GRAY);
 }
 
 void draw_color_picker(const Window& window) {
     DrawRectangleRec(rec_slice_vert(window.color_picker.boundary, 4, 5),
 		     window.draw_color);
     draw_slider(window.color_picker.r);
+    draw_slider(window.color_picker.g);
+    draw_slider(window.color_picker.b);
+    draw_slider(window.color_picker.a);
 }
 
 
 void draw_sprite(const Sprite& sprite, Window& window) {
     DrawTexturePro(sprite.tex, {0.f, 0.f, (float)sprite.tex.width, (float)sprite.tex.height}, sprite.boundary, {0.f, 0.f}, 0.f, WHITE);
-    float cell_size = sprite.boundary.width / sprite.tex.width;
-    Vector2 new_pos = point_to_cell(window.mouse_pos, sprite);
-    DrawRectangle(new_pos.x * cell_size, new_pos.y * cell_size, cell_size, cell_size, WHITE);
+    if (CheckCollisionPointRec(window.mouse_pos, sprite.boundary)) {
+	float cell_size = sprite.boundary.width / sprite.tex.width;
+	Vector2 new_pos = point_to_cell(window.mouse_pos, sprite);
+	DrawRectangle(new_pos.x * cell_size, new_pos.y * cell_size, cell_size, cell_size, window.draw_color);
+    }
 }
 
 void check_slider(Slider& slider, Vector2 mouse_pos) {
@@ -116,6 +135,7 @@ void check_slider(Slider& slider, Vector2 mouse_pos) {
 	    slider.value = (mouse_pos.x - slider.boundary.x) / slider.boundary.width;
 	    DrawText(TextFormat("%d", slider.value), 500, 500, 15, WHITE);
 	    slider.value = Clamp(slider.value, 0.f, 1.f);
+	    slider.handle_rec.x = slider.boundary.x + slider.boundary.width * slider.value - slider.handle_rec.width / 2.f;
 	}
     }
 }
@@ -124,18 +144,21 @@ void controls(Window& window, Sprite& sprite) {
     window.mouse_pos = GetMousePosition();
     if (CheckCollisionPointRec(window.mouse_pos, sprite.boundary)) {
 	if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-	    sprite.set_pixel(point_to_cell(window.mouse_pos, sprite), WHITE);
+	    sprite.set_pixel(point_to_cell(window.mouse_pos, sprite), window.draw_color);
 	}
     }
     if (IsKeyPressed(KEY_S)) {
 	ExportImage(sprite.img, sprite.name);
     }
     check_slider(window.color_picker.r, window.mouse_pos);
+    check_slider(window.color_picker.g, window.mouse_pos);
+    check_slider(window.color_picker.b, window.mouse_pos);
+    check_slider(window.color_picker.a, window.mouse_pos);
 }
 
 int main() {
     Window window = {.width = 1000, .height = 1000, .title = "Sprite Paint"};
-    window.color_picker.setup({window.width / 2.f, 0.f, window.width / 2.f, window.height / 2.f});
+    window.color_picker.setup({window.width / 2.f, 0.f, window.width / 2.f, window.height / 2.f}, window.draw_color);
     init_window(window);
     Sprite sprite;
     sprite.img = GenImageColor(10, 10, BLACK);
@@ -144,10 +167,11 @@ int main() {
     sprite.boundary = {0.f, 0.f, window.width / 2.f, window.height / 2.f};
     while(!WindowShouldClose()) {
 	BeginDrawing();
-	ClearBackground(RED);
+	ClearBackground(GRAY);
 	draw_sprite(sprite, window);
 	draw_color_picker(window);
 	controls(window, sprite);
+	window.draw_color = window.color_picker.to_color();
 	EndDrawing();
     }
     CloseWindow();
